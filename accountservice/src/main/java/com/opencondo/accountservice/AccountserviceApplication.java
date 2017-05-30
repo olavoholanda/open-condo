741ebf462b5ee38424bf769fa3e33526cdb2ed9a
Olavo Holanda <olavo.holanda@meututor.com.br>
2017-05-30 10:19:32 -0300

    First commit with account micro service project

---
+++ accountservice/src/main/java/com/opencondo/accountservice/AccountserviceApplication.java
@@ -0,0 +1,12 @@
+package com.opencondo.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountserviceApplication.class, args);
+	}
+}
