3d27210c1a82bb5acad7e991e70268c09984ece7
Olavo Holanda <olavohcn@gmail.com>
2017-05-31 11:51:29 -0300

    Created an entity called Address for an user
    Following the model of the app, one of the information from users is their address. This address is not a complete one, because a user is already in a condo (that has a full address), therefore, just some information are needed in this address. For house condo, the street name and number. On the other hand, for apartment, the floor and number.

---
+++ accountservice/src/main/java/com/opencondo/accountservice/model/entity/Address.java
@@ -0,0 +1,33 @@
+package com.opencondo.accountservice.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Put a description of the class here.
+ *
+ * @author: olavo
+ */
+@Getter
+@Setter
+public class Address {
+
+    private Boolean house;
+    private String street;
+    private Integer number;
+    private Integer floor;
+
+    @Override
+    public String toString() {
+        if(this.house){
+            return String.format("Address[street=%s, number='%s']",
+                    street, number);
+        } else {
+            return String.format(
+                    "Address[floor=%s, number='%s']",
+                    floor, number);
+        }
+
+
+    }
+}
