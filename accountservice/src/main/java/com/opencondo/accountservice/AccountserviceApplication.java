6bb0f89c02852276b36df0c2dcea69e17a9c99af
Olavo Holanda <olavohcn@gmail.com>
2017-05-31 12:18:00 -0300

    Default spring application class

--- accountservice/src/main/java/com/opencondo/accountservice/AccountserviceApplication.java
+++ accountservice/src/main/java/com/opencondo/accountservice/AccountserviceApplication.java
@@ -1,10 +1,20 @@
 package com.opencondo.accountservice;
 
+import com.opencondo.accountservice.model.storage.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Put a description of the class here.
+ *
+ * @author: Olavo Holanda
+ */
 @SpringBootApplication
 public class AccountserviceApplication {
+
+	@Autowired
+	private UserRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountserviceApplication.class, args);
