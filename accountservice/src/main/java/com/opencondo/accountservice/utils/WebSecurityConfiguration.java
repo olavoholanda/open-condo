bf96fd86b9858f187c601c3426a3b17792ddce97
Olavo Holanda <olavohcn@gmail.com>
2017-06-05 19:15:34 -0300

    Added a Web Security configuration file

---
+++ accountservice/src/main/java/com/opencondo/accountservice/utils/WebSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.opencondo.accountservice.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Put a description of the class here.
+ *
+ * @author: olavo
+ */
+@Configuration
+class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                //.formLogin().and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("user")
+                .authorities("READ")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .authorities("READ", "WRITE");
+    }
+}
