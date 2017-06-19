837c266a2ada8dbfa6ae64ca48b65d09549e00aa
Olavo Holanda <olavohcn@gmail.com>
2017-06-19 11:14:16 -0300

    Simple spring boot app for the forum micro service

---
+++ forumservice/src/main/java/com/opencondo/forumservice/ForumserviceApplication.java
@@ -0,0 +1,12 @@
+package com.opencondo.forumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumserviceApplication.class, args);
+	}
+}
---
+++ forumservice/src/main/java/com/opencondo/forumservice/utils/CORSConfig.java
@@ -0,0 +1,31 @@
+package com.opencondo.forumservice.utils;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Put a description of the class here.
+ *
+ * @author: Olavo Holanda
+ */
+@Configuration
+public class CORSConfig {
+
+  @Bean
+  public FilterRegistrationBean corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(Boolean.TRUE);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+    bean.setOrder(0);
+    return bean;
+  }
+}
