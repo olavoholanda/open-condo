e79adb919551569b49fa2500c62e36384c42e617
Olavo Holanda <olavohcn@gmail.com>
2017-06-21 11:57:23 -0300

    Finished the MessageService with CRUD operations on service layer

---
+++ forumservice/src/main/java/com/opencondo/forumservice/service/MessageService.java
@@ -0,0 +1,96 @@
+package com.opencondo.forumservice.service;
+
+import com.opencondo.forumservice.domain.model.Message;
+import com.opencondo.forumservice.domain.model.Topic;
+import com.opencondo.forumservice.domain.model.UserAccount;
+import com.opencondo.forumservice.domain.repository.MessageRepository;
+import java.util.Date;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * The <code>MessageService</code> class is responsible for CRUD operations regarding
+ * messages. For queries methods look for <code>MessageQueryService</code>. As a service
+ * this class uses only its related repository, in this case <code>MessageRepository</code>,
+ * operations on others entities are performed using the related services.
+ *
+ * @author      Olavo Holanda
+ * @version     0.1
+ * @since       0.1
+ */
+@Service
+public class MessageService {
+
+  private final MessageRepository repository;
+
+  private final UserAccountService userAccountService;
+
+  private final TopicService topicService;
+
+  @Autowired
+  public MessageService(MessageRepository repository, UserAccountService userAccountService,
+      TopicService topicService) {
+    this.repository = repository;
+    this.userAccountService = userAccountService;
+    this.topicService = topicService;
+  }
+
+  /**
+   * Creates a new <code>Message</code> based on the content, topic and author. The message
+   * create time will be the moment that the object is created.
+   *
+   * @param messageContent the <code>String</code> with message's content
+   * @param topicId the <code>Long</code> id of the topic
+   * @param userExternalId the <code>String</code> external id of the author
+   * @return a new, persisted, message
+   */
+  public Message createMessage(String messageContent, Long topicId, String userExternalId) {
+
+    UserAccount author = userAccountService.getUserByExternalId(userExternalId);
+    Topic topic = topicService.getTopicById(topicId);
+
+    Message message = new Message(messageContent, new Date(), topic, author);
+    return repository.save(message);
+  }
+
+  /**
+   * Retrieves the desired <code>Message</code> having the passed id.
+   *
+   * @param id the message <code>Long</code> id
+   * @return the desired message
+   */
+  public Message retrieveMessage(Long id) {
+    Optional<Message> optionalMessage = repository.findById(id);
+    //TODO: throw new exception for not found and remove this null
+    return optionalMessage.orElse(null);
+  }
+
+  /**
+   * Updates the desired <code>Message</code> having the passed id
+   * with the new content
+   *
+   * @param id the message <code>Long</code> id
+   * @param newContent the new message <code>String</code> content
+   * @return the updated desired message
+   */
+  public Message updateMessage(Long id, String newContent) {
+    Message message = this.retrieveMessage(id);
+
+    message.setMessage(newContent);
+    message.setModified(Boolean.TRUE);
+    message.setUpdateTime(new Date());
+
+    return repository.save(message);
+  }
+
+  /**
+   * Deletes the desired <code>Message</code> having the passed id
+   *
+   * @param id the message <code>Long</code> id
+   */
+  public void deleteMessage(Long id) {
+    Message message = this.retrieveMessage(id);
+    repository.delete(message);
+  }
+}
