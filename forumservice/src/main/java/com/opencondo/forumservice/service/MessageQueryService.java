73ebd6455c8dd3708b6a0021115f855f13a426dd
Olavo Holanda <olavohcn@gmail.com>
2017-06-21 12:03:38 -0300

    Finished the MessageQueryService with query methods on messages

---
+++ forumservice/src/main/java/com/opencondo/forumservice/service/MessageQueryService.java
@@ -0,0 +1,44 @@
+package com.opencondo.forumservice.service;
+
+import com.opencondo.forumservice.domain.model.Message;
+import com.opencondo.forumservice.domain.repository.MessageRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+/**
+ * The <code>MessageQueryService</code> class is responsible for query methods regarding
+ * messages. For CRUD operation look for <code>MessageService</code>.
+ *
+ * @author Olavo Holanda
+ * @version 0.1
+ * @since 0.1
+ */
+@Service
+public class MessageQueryService {
+
+  private final MessageRepository repository;
+
+  @Autowired
+  public MessageQueryService(MessageRepository repository) {
+    this.repository = repository;
+  }
+
+  /**
+   * Searches a <code>List</code> of messages related to a topic, sorted by creation time.
+   * The result is paginated by the page number and size.
+   *
+   * @param topicId the <code>Long</code> id of the topic, container of the message.
+   * @param page the <code>Integer</code> with pagination page.
+   * @param size the <code>Integer</code> with pagination size.
+   * @return a list of messages, sorted by creation time.
+   */
+  public List<Message> getLastMessagesByTopicId(Long topicId, Integer page, Integer size) {
+    List<Message> messages = repository.
+        findByTopicId(topicId, PageRequest.of(page, size, Sort.Direction.ASC, "createTime"));
+
+    return messages;
+  }
+}
