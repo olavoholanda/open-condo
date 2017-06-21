0bcb98589e22a77f6a00c721273ee175f4c14fdb
Olavo Holanda <olavohcn@gmail.com>
2017-06-21 10:46:06 -0300

    Removed unnecessary imports

--- forumservice/src/main/java/com/opencondo/forumservice/domain/model/UserAccount.java
+++ forumservice/src/main/java/com/opencondo/forumservice/domain/model/UserAccount.java
@@ -2,13 +2,11 @@ package com.opencondo.forumservice.domain.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
