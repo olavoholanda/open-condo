c41bd4ac2feea8055df6756043c3b9fc576e9c95
Olavo Holanda <olavohcn@gmail.com>
2017-06-26 11:09:12 -0300

    Comment the class explaining the difference between UI id and internal id

--- forumservice/src/main/java/com/opencondo/forumservice/controller/dto/UserDTO.java
+++ forumservice/src/main/java/com/opencondo/forumservice/controller/dto/UserDTO.java
@@ -4,7 +4,9 @@ import com.opencondo.forumservice.domain.model.UserAccount;
 import lombok.Getter;
 
 /**
- * Data Transfer Object for the UserAccount Entity
+ * Data Transfer Object for the UserAccount Entity. For UI convenience
+ * the ID here is the entity external ID and the internalId here is the
+ * entity ID. TODO - What if we use the external ID as the internal ID?
  *
  * @author Olavo Holanda
  * @version 0.1
@@ -15,12 +17,13 @@ public class UserDTO implements DTOMapper<UserAccount> {
 
   private String id;
   private String name;
+  private Long internalId;
 
   @Override
   public String toString() {
     return String.format(
-        "UserAccountDTO[id='%s', name='%s']",
-        id, name);
+        "UserAccountDTO[id='%s', name='%s', internalId=%d]",
+        id, name, internalId);
   }
 
   /**
@@ -32,5 +35,6 @@ public class UserDTO implements DTOMapper<UserAccount> {
   public void buildFromEntity(UserAccount account) {
     this.id = account.getExternalId();
     this.name = account.getName();
+    this.internalId = account.getId();
   }
 }
