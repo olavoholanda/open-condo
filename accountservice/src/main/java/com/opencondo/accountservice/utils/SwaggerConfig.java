e80772c83f9cf369913d83ef350e13b798385321
Olavo Holanda <olavohcn@gmail.com>
2017-11-22 13:22:51 -0300

    Changed the base package for swagger

--- accountservice/src/main/java/com/opencondo/accountservice/utils/SwaggerConfig.java
+++ accountservice/src/main/java/com/opencondo/accountservice/utils/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.basePackage("com.opencondo.forumservice.controller"))
+        .apis(RequestHandlerSelectors.basePackage("com.opencondo.accountservice.controller"))
         .paths(PathSelectors.ant("/api/**"))
         .build();
   }
